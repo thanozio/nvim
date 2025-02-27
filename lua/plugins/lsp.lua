880e24e2c86e0748d2387bea45b28f44ce756470
Thanozio <thanozio@proton.me>
2025-02-27 18:59:49 +0200

    add autocompletions

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -6,9 +6,9 @@ local M = {
         config = function()
             local config_defaults = require("lspconfig").util.default_config
             config_defaults.capabilities = vim.tbl_deep_extend(
-                "force",
-                config_defaults.capabilities,
-                require("cmp_nvim_lsp").default_capabilities()
+            "force",
+            config_defaults.capabilities,
+            require("cmp_nvim_lsp").default_capabilities()
             )
 
             vim.api.nvim_create_autocmd('LspAttach', {
@@ -39,8 +39,67 @@ local M = {
             })
         end
     },
-    { "hrsh7th/cmp-nvim-lsp" },
-    { "hrsh7th/nvim-cmp" }
+    {
+        'hrsh7th/nvim-cmp',
+        event = 'InsertEnter',
+        dependencies = {
+            {
+                'L3MON4D3/LuaSnip',
+                build = (function()
+                    return 'make install_jsregexp'
+                end)(),
+            },
+            'saadparwaiz1/cmp_luasnip',
+            'hrsh7th/cmp-nvim-lsp',
+            'hrsh7th/cmp-path',
+            'hrsh7th/cmp-nvim-lsp-signature-help',
+        },
+        config = function()
+            local cmp = require 'cmp'
+            local luasnip = require 'luasnip'
+            luasnip.config.setup {}
+
+            cmp.setup {
+                snippet = {
+                    expand = function(args)
+                        luasnip.lsp_expand(args.body)
+                    end,
+                },
+                completion = { completeopt = 'menu,menuone,noinsert' },
+
+                mapping = cmp.mapping.preset.insert {
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<CR>'] = cmp.mapping.confirm { select = true },
+                    ['<Tab>'] = cmp.mapping.select_next_item(),
+                    ['<S-Tab>'] = cmp.mapping.select_prev_item(),
+                    ['<C-Space>'] = cmp.mapping.complete {},
+
+                    ['<C-l>'] = cmp.mapping(function()
+                        if luasnip.expand_or_locally_jumpable() then
+                            luasnip.expand_or_jump()
+                        end
+                    end, { 'i', 's' }),
+                    ['<C-h>'] = cmp.mapping(function()
+                        if luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        end
+                    end, { 'i', 's' }),
+
+                },
+                sources = {
+                    {
+                        name = 'lazydev',
+                        group_index = 0,
+                    },
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    { name = 'path' },
+                    { name = 'nvim_lsp_signature_help' },
+                },
+            }
+        end,
+    },
 }
 
 
